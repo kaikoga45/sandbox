e39c2907bd1cbe6ba331de94c4abeee5e1f139be
kaikoga45 <koga.kai@icloud.com>
2024-07-04 13:21:40 +0700

    1

---
+++ lib/presentation/pages/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void _throwError() => throw Exception('Error @ ${DateTime.now()}');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _throwError,
+        child: const Icon(Icons.bug_report),
+      ),
+    );
+  }
+}
