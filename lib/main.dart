f119aa1d384d2c763b38584859628bbfb76c3da5
Kai Koga <koga.kai@icloud.com>
2024-10-28 14:59:54 +0700

    12

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:sandbox/firebase_options.dart';
 import 'package:sandbox/presentation/pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initialize();
-  FlutterError.onError = (errorDetails) {
-    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
-  };
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
   runApp(const MyApp());
-}
-
-Future<void> initialize() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 }
 
 class MyApp extends StatelessWidget {
